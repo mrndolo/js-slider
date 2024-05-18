0d8faad33b1318c7b60f9e52ee101ac6599ccbf2
mrndolo <callistomutunga@gmail.com>
2024-05-18 09:29:54 +0300

    Implemented right button logic

---
+++ script.js
@@ -0,0 +1,21 @@
+const left = document.querySelector(".left")
+const right = document.querySelector(".right")
+const slider = document.querySelector(".slider")
+const images = document.querySelectorAll(".image")
+
+let slideNumber = 1;
+const length = images.length;
+
+const nextSlide = ()=>{
+    slider.style.transform = `translateX(-${slideNumber * 500}px)`;
+    slideNumber ++;
+}
+const prevSlide = ()=>{
+    slider.style.transform = `translateX(0px)`;
+    slideNumber = 1;
+}
+
+right.addEventListener('click', ()=>{
+    // console.log('Clicked!')
+    slideNumber < length ? nextSlide() : prevSlide();
+    });
